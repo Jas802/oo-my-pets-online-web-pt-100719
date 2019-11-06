c9314c73fde27070bf76632ad384dd0a77aea8f4
Jas Spencer <jas.spencer414@gmail.com>
2019-11-06 13:27:41 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
 class Owner
-  # code goes here
+attr_reader :name, :species
+@@all = []
+
+def initialize(name)
+  @name = name
+  @speices = "humman"
+  @@all << self
+end
+
+def buy_cat(name)
+  Cat.new(name, self)
+
+  def buy_dog(name)
+    Dog.new(name, self)
 end
